117a0b5e0ea6b13edc72333e298377f1f272d046
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2023-11-30 22:58:17 +0300

    hero

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -10,14 +10,21 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className="w-full flex xl:flex-row  justify-center min-h-screen gap-10 max-container "
+      className="w-full flex xl:flex-row  justify-center min-h-screen 
+      gap-10 max-container "
     >
-      <div className="relative xl:w-2/5 flex flex-col justify-center  min-h-screen gap-10 max-container  ">
+      <div
+        className="relative xl:w-2/5 flex flex-col justify-center 
+       min-h-screen gap-10 max-container  "
+      >
         <p className="text-xl font-montserrat text-coral-red">
           Our Summer collections
         </p>
 
-        <h1 className=" mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold   ">
+        <h1
+          className=" mt-10 font-palanquin text-8xl max-sm:text-[72px]
+         max-sm:leading-[82px] font-bold   "
+        >
           <span className="xl:bg-white xl:whitespace-normal relative z-10 pr-10  ">
             The New Arrival
           </span>
@@ -45,7 +52,10 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center   ">
+      <div
+        className="relative flex-1 flex justify-center items-center
+       xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center   "
+      >
         <img
           src={bigShoeImg}
           alt="shoe colletion"
@@ -54,7 +64,10 @@ const Hero = () => {
           className="object-contain relative z-10 "
         />
 
-        <div className="flex sm:gap-6 absolute  -bottom-[5%] sm:left-[10%] max-sm:px-6">
+        <div
+          className="flex sm:gap-6 absolute  -bottom-[5%] 
+        sm:left-[10%] max-sm:px-6"
+        >
           {shoes.map((image, index) => (
             <div key={index}>
               <ShoeCard
