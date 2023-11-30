58f3132a71466f625e1d1f04fbd750046eea38c4
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2023-11-30 23:57:08 +0300

    Services

--- src/Sections/Services.jsx
+++ src/Sections/Services.jsx
@@ -1,5 +1,16 @@
+import { services } from "../constants";
+import { ServiceCard } from "../components";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section>
+      <div className=" max-container flex  justify-center flex-wrap gap-9  ">
+        {services.map((service) => (
+          <ServiceCard key={service.label} {...service} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Services;
--- src/Sections/SpecialOffer.jsx
+++ src/Sections/SpecialOffer.jsx
@@ -1,5 +1,9 @@
 const SpecialOffer = () => {
-  return <div>SpecialOffer</div>;
+  return (
+    <section>
+      <div className=" max-container flex  justify-center flex-wrap gap-9  "></div>
+    </section>
+  );
 };
 
 export default SpecialOffer;
