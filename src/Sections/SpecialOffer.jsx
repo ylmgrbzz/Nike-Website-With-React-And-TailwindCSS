164c80317b27834fc311e0c4f7ff1465b40e0f18
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2023-12-01 00:07:32 +0300

    SpecialOffer

--- src/Sections/SpecialOffer.jsx
+++ src/Sections/SpecialOffer.jsx
@@ -1,7 +1,35 @@
+import { arrowRight } from "../assets/icons";
+import { offer } from "../assets/images";
+import { Button } from "../components";
+
 const SpecialOffer = () => {
   return (
     <section>
-      <div className=" max-container flex  justify-center flex-wrap gap-9  "></div>
+      <div className="  ">
+        <div>
+          <img />
+        </div>
+        <div>
+          <h2 className="text-4xl font-palanquin font-bold">
+            <span className="text-coral-red">Special </span>
+            Offer
+          </h2>
+          <p className="mt-4 info-text">
+            Embark on a shopping journey that redefines your experience with
+            unbeatable deals. From premier selections to incredible savings, we
+            offer unparalleled value that sets us apart.
+          </p>
+          <p className="mt-6 info-text">
+            Navigate a realm of possibilities designed to fulfill your unique
+            desires, surpassing the loftiest expectations. Your journey with us
+            is nothing short of exceptional.
+          </p>
+        </div>
+        <div>
+          <Button />
+          <Button />
+        </div>
+      </div>
     </section>
   );
 };
